07296d27f9707a451fe15b0adcb00dcce7b5f953
Raynaldi Zulfikar <raynaldizoel@gmail.com>
2023-12-11 11:14:03 +0700

    Update Apps

--- app/src/main/java/com/app/stoikapp/view/home/HomeFragment.kt
+++ app/src/main/java/com/app/stoikapp/view/home/HomeFragment.kt
@@ -1,24 +1,36 @@
 package com.app.stoikapp.view.home
 
+import SoundAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.stoikapp.R
 import com.app.stoikapp.data.datastore.SharedPref
+import com.app.stoikapp.data.model.MeditasiSong
+import com.app.stoikapp.data.model.Psikolog
 import com.app.stoikapp.databinding.FragmentHomeBinding
+import com.app.stoikapp.view.home.adapter.HomePsikologAdapter
+import com.app.stoikapp.view.home.psikolog.adapter.PsikologAdapter
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class HomeFragment : Fragment() {
-    lateinit var binding : FragmentHomeBinding
-    val auth = FirebaseAuth.getInstance()
-    lateinit var sharedPref: SharedPref
+    private lateinit var binding: FragmentHomeBinding
+    private val auth = FirebaseAuth.getInstance()
+    private lateinit var sharedPref: SharedPref
+    private lateinit var databaseReferenceMeditasi: DatabaseReference
+    private lateinit var databaseReferencePsikolog: DatabaseReference
+    private lateinit var soundAdapter: SoundAdapter
+    private lateinit var psikologAdapter: HomePsikologAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -26,7 +38,68 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         sharedPref = SharedPref(requireContext())
+        databaseReferenceMeditasi = FirebaseDatabase.getInstance().getReference("meditasi")
+        databaseReferencePsikolog = FirebaseDatabase.getInstance().getReference("psikolog")
 
+        binding.soundRelax.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        binding.psikologTerdekat.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        soundAdapter = SoundAdapter(requireContext(), mutableListOf())
+        psikologAdapter = HomePsikologAdapter(requireContext(), mutableListOf())
+
+        binding.soundRelax.adapter = soundAdapter
+        binding.psikologTerdekat.adapter = psikologAdapter
+
+        fetchDataFromFirebaseMeditasi()
+        fetchDataFromFirebasePsikolog()
+
+        binding.btnDetailSound.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_soundMeditasiFragment)
+        }
+
+        binding.btnDetailPsikolog.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_psikologFragment)
+        }
     }
 
+    private fun fetchDataFromFirebaseMeditasi() {
+        databaseReferenceMeditasi.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val meditasiList = mutableListOf<MeditasiSong>()
+
+                for (childSnapshot in snapshot.children) {
+                    val meditasiData = childSnapshot.getValue(MeditasiSong::class.java)
+                    meditasiData?.let {
+                        meditasiList.add(it)
+                    }
+                }
+
+                soundAdapter.updateData(meditasiList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+
+    private fun fetchDataFromFirebasePsikolog() {
+        databaseReferencePsikolog.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val psikologList = mutableListOf<Psikolog>()
+
+                for (childSnapshot in snapshot.children) {
+                    val psikologData = childSnapshot.getValue(Psikolog::class.java)
+                    psikologData?.let {
+                        psikologList.add(it)
+                    }
+                }
+
+                psikologAdapter.updateData(psikologList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
 }
